39c97532ac51f5cfacd62d94e723955ef621ed15
0xRUDEBOY <116237237+0xRUDEBOY@users.noreply.github.com>
2023-12-27 19:13:08 +0400

    fix  3

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
-import { fadeIn, textVariant } from "../utils/motion";
+import { fadeIn, textVariant } from "../Utils/motion";
 import { testimonials } from "../constants";
 
 const FeedbackCard = ({
